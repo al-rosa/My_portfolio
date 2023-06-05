4709bfa31c30492166b8fe609a5d453c96442fd3
al-rosa <alan21delarosa21@gnail.com>
2023-06-05 22:32:53 +0900

    fix:top content posi

--- lib/view/section/Top/top_v2.dart
+++ lib/view/section/Top/top_v2.dart
@@ -13,6 +13,10 @@ class TopV2 extends StatelessWidget {
     final Size screen = ResponsiveWidget.getScreenSize(context);
     final bool isSmall = ResponsiveWidget.isSmallScreen(context);
 
+    final EdgeInsetsGeometry contentPadding = screen.width < 600
+        ? const EdgeInsets.all(14)
+        : EdgeInsets.only(left: screen.width * 0.25);
+
     return Container(
       width: screen.width,
       height: screen.height,
@@ -20,117 +24,111 @@ class TopV2 extends StatelessWidget {
         color: IColor.background,
       ),
       child: Padding(
-        padding: const EdgeInsets.all(14.0),
+        padding: contentPadding,
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Container(
-              width: 180,
-              height: 180,
-              decoration: BoxDecoration(
-                shape: BoxShape.circle,
-                image: DecorationImage(
-                  fit: BoxFit.fill,
-                  image: AssetImage(
-                    path("r0sa.png"),
+            Padding(
+              padding: isSmall
+                  ? EdgeInsets.only(left: screen.width / 5)
+                  : EdgeInsets.only(left: screen.width / 20),
+              child: Container(
+                width: 180,
+                height: 180,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  image: DecorationImage(
+                    fit: BoxFit.fill,
+                    image: AssetImage(
+                      path("r0sa.png"),
+                    ),
                   ),
                 ),
               ),
             ),
             SizedBox(height: screen.height / 17),
-            Padding(
-              padding: isSmall
-                  ? EdgeInsets.zero
-                  : EdgeInsets.only(left: screen.width * 0.35),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Row(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      VerticalStick(
-                        child: Text(
-                          "r0sa",
-                          style:
-                              isSmall ? ITextStyle.boldText : ITextStyle.title,
-                        ),
-                      ),
-                      const SizedBox(width: 12),
-                      CircleAvatar(
-                        radius: isSmall ? 18 : 30,
-                        backgroundColor: IColor.textColor,
-                      ),
-                      const SizedBox(width: 12),
-                      CircleAvatar(
-                        radius: isSmall ? 18 : 30,
-                        backgroundColor: IColor.blue,
-                      ),
-                      const SizedBox(width: 12),
-                      CircleAvatar(
-                        radius: isSmall ? 18 : 30,
-                        backgroundColor: IColor.green,
-                      ),
-                      const SizedBox(width: 12),
-                      CircleAvatar(
-                        radius: isSmall ? 18 : 30,
-                        backgroundColor: IColor.brown,
-                      ),
-                    ],
+            Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                VerticalStick(
+                  child: Text(
+                    "r0sa",
+                    style: isSmall ? ITextStyle.boldText : ITextStyle.title,
                   ),
-                  const SizedBox(height: 12),
-                  const Text("関西のFlutterエンジニア。", style: ITextStyle.subTitle),
-                  const SizedBox(height: 22),
-                  Row(
-                    mainAxisSize: MainAxisSize.min,
-                    children: const [
-                      CircleAvatar(
-                        radius: 12,
-                        backgroundColor: IColor.blue,
-                      ),
-                      SizedBox(width: 12),
-                      Expanded(
-                        child: Text(
-                          "Engineering：Fluuter,React,Go,Firebase",
-                          style: ITextStyle.regularText,
-                        ),
-                      )
-                    ],
+                ),
+                const SizedBox(width: 12),
+                CircleAvatar(
+                  radius: isSmall ? 18 : 30,
+                  backgroundColor: IColor.textColor,
+                ),
+                const SizedBox(width: 12),
+                CircleAvatar(
+                  radius: isSmall ? 18 : 30,
+                  backgroundColor: IColor.blue,
+                ),
+                const SizedBox(width: 12),
+                CircleAvatar(
+                  radius: isSmall ? 18 : 30,
+                  backgroundColor: IColor.green,
+                ),
+                const SizedBox(width: 12),
+                CircleAvatar(
+                  radius: isSmall ? 18 : 30,
+                  backgroundColor: IColor.brown,
+                ),
+              ],
+            ),
+            const SizedBox(height: 12),
+            const Text("関西のFlutterエンジニア。", style: ITextStyle.subTitle),
+            const SizedBox(height: 22),
+            Row(
+              mainAxisSize: MainAxisSize.min,
+              children: const [
+                CircleAvatar(
+                  radius: 12,
+                  backgroundColor: IColor.blue,
+                ),
+                SizedBox(width: 12),
+                Expanded(
+                  child: Text(
+                    "Engineering：Fluuter,React,Go,Firebase",
+                    style: ITextStyle.regularText,
                   ),
-                  const SizedBox(height: 12),
-                  Row(
-                    mainAxisSize: MainAxisSize.min,
-                    children: const [
-                      CircleAvatar(
-                        radius: 12,
-                        backgroundColor: IColor.green,
-                      ),
-                      SizedBox(width: 12),
-                      Text(
-                        "Design：Figma,graphic",
-                        style: ITextStyle.regularText,
-                      )
-                    ],
+                )
+              ],
+            ),
+            const SizedBox(height: 12),
+            Row(
+              mainAxisSize: MainAxisSize.min,
+              children: const [
+                CircleAvatar(
+                  radius: 12,
+                  backgroundColor: IColor.green,
+                ),
+                SizedBox(width: 12),
+                Text(
+                  "Design：Figma,graphic",
+                  style: ITextStyle.regularText,
+                )
+              ],
+            ),
+            const SizedBox(height: 12),
+            Row(
+              mainAxisSize: MainAxisSize.min,
+              children: const [
+                CircleAvatar(
+                  radius: 12,
+                  backgroundColor: IColor.brown,
+                ),
+                SizedBox(width: 12),
+                Expanded(
+                  child: Text(
+                    "Hobbies：watching movies,painting,petting cats.",
+                    style: ITextStyle.regularText,
                   ),
-                  const SizedBox(height: 12),
-                  Row(
-                    mainAxisSize: MainAxisSize.min,
-                    children: const [
-                      CircleAvatar(
-                        radius: 12,
-                        backgroundColor: IColor.brown,
-                      ),
-                      SizedBox(width: 12),
-                      Expanded(
-                        child: Text(
-                          "Hobbies：watching movies,painting,petting cats.",
-                          style: ITextStyle.regularText,
-                        ),
-                      )
-                    ],
-                  ),
-                ],
-              ),
+                )
+              ],
             ),
           ],
         ),
